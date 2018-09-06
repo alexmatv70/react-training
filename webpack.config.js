d48ce8d973f03d82c7e9259113d68991ac77e643
oleksandr.matviishen <oleksandr.matviishen@udelphi.com>
2018-09-06 18:07:30 +0300

    Simplified webpack.config.js without parts merging.

--- webpack.config.js
+++ webpack.config.js
@@ -1,110 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const merge = require('webpack-merge');
 
-const publicPath = '/';
-const title = 'Sports Store';
-
-const parts = {
-  base: {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'index.js',
-      publicPath,
-    },
-    resolve: {
-      modules: ['node_modules', './src'],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title,
-        inject: true,
-        favicon: './src/favicon.png',
-        template: './src/index.ejs',
-      }),
-      new ManifestPlugin({
-        fileName: 'asset-manifest.json',
-      }),
-      new WebpackPwaManifest({
-        inject: true,
-        name: title,
-        short_name: title,
-        description: title,
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        start_url: './index.html',
-        display: 'standalone',
-        icons: [
-          {
-            src: path.resolve('src/favicon.png'),
-            size: 192,
-          },
-        ],
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          use: ['babel-loader'],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.(eot|otf|svg|ttf|woff|png)$/,
-          use: {
-            loader: 'file-loader',
-            options: { name: 'assets/[name][hash].[ext]' },
-          },
-        },
-      ],
-    },
-    devtool: 'source-map',
-    node: {
-      dgram: 'empty',
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      child_process: 'empty',
-    },
+module.exports = (_, { mode }) => ({
+  mode,
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    publicPath: '/',
   },
-
-  development: {
-    mode: 'development',
-    devtool: 'cheap-module-source-map',
-    devServer: {
-      contentBase: false,
-      historyApiFallback: true,
-      host: '0.0.0.0',
-      port: 3000,
-    },
+  resolve: {
+    modules: ['node_modules', './src'],
+    extensions: ['.js', '.jsx', '.json'],
   },
-
-  production: {
-    mode: 'production',
-    plugins: [
-      new HtmlWebpackPlugin({
-        title,
-        inject: true,
-        favicon: './src/favicon.png',
-        template: './src/index.ejs',
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Sports Store',
+      inject: true,
+      favicon: './src/favicon.png',
+      template: './src/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(eot|otf|svg|ttf|woff|png)$/,
+        use: {
+          loader: 'file-loader',
+          options: { name: 'assets/[name][hash].[ext]' },
         },
-      }),
+      },
     ],
   },
-};
-
-module.exports = merge(parts.base, parts[process.env.NODE_ENV]);
+  devtool: mode === 'production' ? false : 'cheap-module-source-map',
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+  devServer: {
+    contentBase: false,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: 1000,
+  },
+});
